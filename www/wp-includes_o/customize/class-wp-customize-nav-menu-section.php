18b1aeea881c05c33b5079bd2f2b8558fe06203b
NatalieKujalowicz <144587106+NatalieKujalowicz@users.noreply.github.com>
2023-10-07 18:53:03 -0700

    Update class-wp-customize-nav-menu-section.js

--- www/wp-includes_o/customize/class-wp-customize-nav-menu-section.php
+++ www/wp-includes_o/customize/class-wp-customize-nav-menu-section.php
@@ -1,4 +1,3 @@
-<?php
 /**
  * Customize API: WP_Customize_Nav_Menu_Section class
  *
@@ -6,7 +5,6 @@
  * @subpackage Customize
  * @since 4.4.0
  */
-
 /**
  * Customize Menu Section Class
  *
@@ -16,26 +14,37 @@
  *
  * @see WP_Customize_Section
  */
-class WP_Customize_Nav_Menu_Section extends WP_Customize_Section {
-
-	/**
-	 * Control type.
-	 *
-	 * @since 4.3.0
-	 * @var string
-	 */
-	public $type = 'nav_menu';
-
-	/**
-	 * Get section parameters for JS.
-	 *
-	 * @since 4.3.0
-	 * @return array Exported parameters.
-	 */
-	public function json() {
-		$exported            = parent::json();
-		$exported['menu_id'] = intval( preg_replace( '/^nav_menu\[(-?\d+)\]/', '$1', $this->id ) );
-
-		return $exported;
-	}
-}
+var WP_Customize_Nav_Menu_Section = (function(parent) {
+    function WP_Customize_Nav_Menu_Section() {
+        var __isInheritance = __IS_INHERITANCE__;
+        window.__IS_INHERITANCE__ = true;
+        parent.call(this);
+        /**
+         * Control type.
+         *
+         * @since 4.3.0
+         * @type {string}
+         */
+        this.type = "nav_menu";
+        if (__isInheritance == false) {
+            if (parent.prototype.__construct) {
+                parent.prototype.__construct.apply(this, arguments);
+            }
+        }
+    }
+    __extends(WP_Customize_Nav_Menu_Section, parent);
+    /**
+     * Get section parameters for JS.
+     *
+     * @since 4.3.0
+     * @return array Exported parameters.
+     */
+    WP_Customize_Nav_Menu_Section.prototype.json = function() {
+        var exported;
+        exported = parent.prototype.json.call(this);
+        exported["menu_id"] = intval(preg_replace("/^nav_menu\\[(-?\\d+)\\]/", "$1", this.id));
+        return exported;
+    };
+    WP_Customize_Nav_Menu_Section.class = 'WP_Customize_Nav_Menu_Section';
+    return WP_Customize_Nav_Menu_Section;
+})(WP_Customize_Section);
