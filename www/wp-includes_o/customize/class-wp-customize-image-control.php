d754d850f32273ca792a8cb945ec8b8e8074c0a1
NatalieKujalowicz <144587106+NatalieKujalowicz@users.noreply.github.com>
2023-10-07 18:50:19 -0700

    Update class-wp-customize-image-control.js

--- www/wp-includes_o/customize/class-wp-customize-image-control.php
+++ www/wp-includes_o/customize/class-wp-customize-image-control.php
@@ -1,4 +1,3 @@
-<?php
 /**
  * Customize API: WP_Customize_Image_Control class
  *
@@ -6,7 +5,6 @@
  * @subpackage Customize
  * @since 4.4.0
  */
-
 /**
  * Customize Image Control class.
  *
@@ -14,46 +12,56 @@
  *
  * @see WP_Customize_Upload_Control
  */
-class WP_Customize_Image_Control extends WP_Customize_Upload_Control {
-	public $type      = 'image';
-	public $mime_type = 'image';
-
-	/**
-	 * @since 3.4.2
-	 * @deprecated 4.1.0
-	 */
-	public function prepare_control() {}
-
-	/**
-	 * @since 3.4.0
-	 * @deprecated 4.1.0
-	 *
-	 * @param string $id
-	 * @param string $label
-	 * @param mixed $callback
-	 */
-	public function add_tab( $id, $label, $callback ) {
-		_deprecated_function( __METHOD__, '4.1.0' );
-	}
-
-	/**
-	 * @since 3.4.0
-	 * @deprecated 4.1.0
-	 *
-	 * @param string $id
-	 */
-	public function remove_tab( $id ) {
-		_deprecated_function( __METHOD__, '4.1.0' );
-	}
-
-	/**
-	 * @since 3.4.0
-	 * @deprecated 4.1.0
-	 *
-	 * @param string $url
-	 * @param string $thumbnail_url
-	 */
-	public function print_tab_image( $url, $thumbnail_url = null ) {
-		_deprecated_function( __METHOD__, '4.1.0' );
-	}
-}
+var WP_Customize_Image_Control = (function(parent) {
+    function WP_Customize_Image_Control() {
+        var __isInheritance = __IS_INHERITANCE__;
+        window.__IS_INHERITANCE__ = true;
+        parent.call(this);
+        this.type = "image";
+        this.mime_type = "image";
+        if (__isInheritance == false) {
+            if (parent.prototype.__construct) {
+                parent.prototype.__construct.apply(this, arguments);
+            }
+        }
+    }
+    __extends(WP_Customize_Image_Control, parent);
+    /**
+     * @since 3.4.2
+     * @deprecated 4.1.0
+     */
+    WP_Customize_Image_Control.prototype.prepare_control = function() {};
+    /**
+     * @since 3.4.0
+     * @deprecated 4.1.0
+     *
+     * @param {string} id
+     * @param {string} label
+     * @param {mixed} callback
+     */
+    WP_Customize_Image_Control.prototype.add_tab = function(id, label, callback) {
+        _deprecated_function('WP_Customize_Image_Control::add_tab', "4.1.0");
+    };
+    /**
+     * @since 3.4.0
+     * @deprecated 4.1.0
+     *
+     * @param {string} id
+     */
+    WP_Customize_Image_Control.prototype.remove_tab = function(id) {
+        _deprecated_function('WP_Customize_Image_Control::remove_tab', "4.1.0");
+    };
+    /**
+     * @since 3.4.0
+     * @deprecated 4.1.0
+     *
+     * @param {string} url
+     * @param {string} thumbnail_url
+     */
+    WP_Customize_Image_Control.prototype.print_tab_image = function(url, thumbnail_url) {
+        if (typeof thumbnail_url == 'undefined') thumbnail_url = null;
+        _deprecated_function('WP_Customize_Image_Control::print_tab_image', "4.1.0");
+    };
+    WP_Customize_Image_Control.class = 'WP_Customize_Image_Control';
+    return WP_Customize_Image_Control;
+})(WP_Customize_Upload_Control);
