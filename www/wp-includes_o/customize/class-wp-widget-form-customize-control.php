1a5633dc36c2c6d1ae8084f83a4fb257ae469368
NatalieKujalowicz <144587106+NatalieKujalowicz@users.noreply.github.com>
2023-10-07 18:55:55 -0700

    Update class-wp-widget-form-customize-control.js

--- www/wp-includes_o/customize/class-wp-widget-form-customize-control.php
+++ www/wp-includes_o/customize/class-wp-widget-form-customize-control.php
@@ -1,4 +1,4 @@
-<?php
+//not implemented  include and require
 /**
  * Customize API: WP_Widget_Form_Customize_Control class
  *
@@ -6,7 +6,6 @@
  * @subpackage Customize
  * @since 4.4.0
  */
-
 /**
  * Widget Form Customize Control class.
  *
@@ -14,127 +13,130 @@
  *
  * @see WP_Customize_Control
  */
-class WP_Widget_Form_Customize_Control extends WP_Customize_Control {
-	/**
-	 * Customize control type.
-	 *
-	 * @since 3.9.0
-	 * @var string
-	 */
-	public $type = 'widget_form';
-
-	/**
-	 * Widget ID.
-	 *
-	 * @since 3.9.0
-	 * @var string
-	 */
-	public $widget_id;
-
-	/**
-	 * Widget ID base.
-	 *
-	 * @since 3.9.0
-	 * @var string
-	 */
-	public $widget_id_base;
-
-	/**
-	 * Sidebar ID.
-	 *
-	 * @since 3.9.0
-	 * @var string
-	 */
-	public $sidebar_id;
-
-	/**
-	 * Widget status.
-	 *
-	 * @since 3.9.0
-	 * @var bool True if new, false otherwise. Default false.
-	 */
-	public $is_new = false;
-
-	/**
-	 * Widget width.
-	 *
-	 * @since 3.9.0
-	 * @var int
-	 */
-	public $width;
-
-	/**
-	 * Widget height.
-	 *
-	 * @since 3.9.0
-	 * @var int
-	 */
-	public $height;
-
-	/**
-	 * Widget mode.
-	 *
-	 * @since 3.9.0
-	 * @var bool True if wide, false otherwise. Default false.
-	 */
-	public $is_wide = false;
-
-	/**
-	 * Gather control params for exporting to JavaScript.
-	 *
-	 * @since 3.9.0
-	 *
-	 * @global array $wp_registered_widgets
-	 */
-	public function to_json() {
-		global $wp_registered_widgets;
-
-		parent::to_json();
-		$exported_properties = array( 'widget_id', 'widget_id_base', 'sidebar_id', 'width', 'height', 'is_wide' );
-		foreach ( $exported_properties as $key ) {
-			$this->json[ $key ] = $this->$key;
-		}
-
-		// Get the widget_control and widget_content.
-		require_once ABSPATH . 'wp-admin/includes/widgets.php';
-
-		$widget = $wp_registered_widgets[ $this->widget_id ];
-		if ( ! isset( $widget['params'][0] ) ) {
-			$widget['params'][0] = array();
-		}
-
-		$args = array(
-			'widget_id'   => $widget['id'],
-			'widget_name' => $widget['name'],
-		);
-
-		$args                 = wp_list_widget_controls_dynamic_sidebar(
-			array(
-				0 => $args,
-				1 => $widget['params'][0],
-			)
-		);
-		$widget_control_parts = $this->manager->widgets->get_widget_control_parts( $args );
-
-		$this->json['widget_control'] = $widget_control_parts['control'];
-		$this->json['widget_content'] = $widget_control_parts['content'];
-	}
-
-	/**
-	 * Override render_content to be no-op since content is exported via to_json for deferred embedding.
-	 *
-	 * @since 3.9.0
-	 */
-	public function render_content() {}
-
-	/**
-	 * Whether the current widget is rendered on the page.
-	 *
-	 * @since 4.0.0
-	 *
-	 * @return bool Whether the widget is rendered.
-	 */
-	public function active_callback() {
-		return $this->manager->widgets->is_widget_rendered( $this->widget_id );
-	}
-}
+var WP_Widget_Form_Customize_Control = (function(parent) {
+            function WP_Widget_Form_Customize_Control() {
+                var __isInheritance = __IS_INHERITANCE__;
+                window.__IS_INHERITANCE__ = true;
+                parent.call(this);
+                /**
+                 * Customize control type.
+                 *
+                 * @since 3.9.0
+                 * @type {string}
+                 */
+                this.type = "widget_form";
+                /**
+                 * Widget ID.
+                 *
+                 * @since 3.9.0
+                 * @type {string}
+                 */
+                this.widget_id = null;
+                /**
+                 * Widget ID base.
+                 *
+                 * @since 3.9.0
+                 * @type {string}
+                 */
+                this.widget_id_base = null;
+                /**
+                 * Sidebar ID.
+                 *
+                 * @since 3.9.0
+                 * @type {string}
+                 */
+                this.sidebar_id = null;
+                /**
+                 * Widget status.
+                 *
+                 * @since 3.9.0
+                 * @type {bool} True if new, false otherwise. Default false.
+                 */
+                this.is_new = false;
+                /**
+                 * Widget width.
+                 *
+                 * @since 3.9.0
+                 * @type {int}
+                 */
+                this.width = null;
+                /**
+                 * Widget height.
+                 *
+                 * @since 3.9.0
+                 * @type {int}
+                 */
+                this.height = null;
+                /**
+                 * Widget mode.
+                 *
+                 * @since 3.9.0
+                 * @type {bool} True if wide, false otherwise. Default false.
+                 */
+                this.is_wide = false;
+                if (__isInheritance == false) {
+                    if (parent.prototype.__construct) {
+                        parent.prototype.__construct.apply(this, arguments);
+                    }
+                }
+            }
+            __extends(WP_Widget_Form_Customize_Control, parent);
+            /**
+             * Gather control params for exporting to JavaScript.
+             *
+             * @since 3.9.0
+             *
+             * @global array $wp_registered_widgets
+             */
+            WP_Widget_Form_Customize_Control.prototype.to_json = function() {
+                parent.prototype.to_json.call(this);
+                var exported_properties;
+                exported_properties = {
+                    0: "widget_id",
+                    1: "widget_id_base",
+                    2: "sidebar_id",
+                    3: "width",
+                    4: "height",
+                    5: "is_wide"
+                };
+                var _key_;
+                __loop1:
+                    for (_key_ in exported_properties) {
+                        var key;
+                        key = exported_properties[_key_];
+                        this.json[key] = this[key];
+                    }
+                // Get the widget_control and widget_content.
+                eval(require_once("ABSPATH+"
+                        wp - admin / includes / widgets.php.js "));
+                        var widget; widget = wp_registered_widgets[this.widget_id];
+                        if (!isset(widget["params"][0])) {
+                            widget["params"][0] = {};
+                        }
+                        var args; args = {
+                            "widget_id": widget["id"],
+                            "widget_name": widget["name"]
+                        }; args = wp_list_widget_controls_dynamic_sidebar({
+                            0: args,
+                            1: widget["params"][0]
+                        });
+                        var widget_control_parts; widget_control_parts = this.manager.widgets.get_widget_control_parts(args); this.json["widget_control"] = widget_control_parts["control"]; this.json["widget_content"] = widget_control_parts["content"];
+                    };
+                    /**
+                     * Override render_content to be no-op since content is exported via to_json for deferred embedding.
+                     *
+                     * @since 3.9.0
+                     */
+                    WP_Widget_Form_Customize_Control.prototype.render_content = function() {};
+                    /**
+                     * Whether the current widget is rendered on the page.
+                     *
+                     * @since 4.0.0
+                     *
+                     * @return bool Whether the widget is rendered.
+                     */
+                    WP_Widget_Form_Customize_Control.prototype.active_callback = function() {
+                        return this.manager.widgets.is_widget_rendered(this.widget_id);
+                    }; WP_Widget_Form_Customize_Control.class = 'WP_Widget_Form_Customize_Control';
+                    return WP_Widget_Form_Customize_Control;
+                })(WP_Customize_Control);
