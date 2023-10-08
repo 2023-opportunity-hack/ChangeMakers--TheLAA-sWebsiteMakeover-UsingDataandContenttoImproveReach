6254074fd9c1957c83923104392b2426e5228757
NatalieKujalowicz <144587106+NatalieKujalowicz@users.noreply.github.com>
2023-10-07 18:42:55 -0700

    Update class-wp-customize-background-image-control.js

--- www/wp-includes_o/customize/class-wp-customize-background-image-control.php
+++ www/wp-includes_o/customize/class-wp-customize-background-image-control.php
@@ -1,4 +1,3 @@
-<?php
 /**
  * Customize API: WP_Customize_Background_Image_Control class
  *
@@ -6,7 +5,6 @@
  * @subpackage Customize
  * @since 4.4.0
  */
-
 /**
  * Customize Background Image Control class.
  *
@@ -14,46 +12,47 @@
  *
  * @see WP_Customize_Image_Control
  */
-class WP_Customize_Background_Image_Control extends WP_Customize_Image_Control {
-	public $type = 'background';
-
-	/**
-	 * Constructor.
-	 *
-	 * @since 3.4.0
-	 * @uses WP_Customize_Image_Control::__construct()
-	 *
-	 * @param WP_Customize_Manager $manager Customizer bootstrap instance.
-	 */
-	public function __construct( $manager ) {
-		parent::__construct(
-			$manager,
-			'background_image',
-			array(
-				'label'   => __( 'Background Image' ),
-				'section' => 'background_image',
-			)
-		);
-	}
-
-	/**
-	 * Enqueue control related scripts/styles.
-	 *
-	 * @since 4.1.0
-	 */
-	public function enqueue() {
-		parent::enqueue();
-
-		$custom_background = get_theme_support( 'custom-background' );
-		wp_localize_script(
-			'customize-controls',
-			'_wpCustomizeBackground',
-			array(
-				'defaults' => ! empty( $custom_background[0] ) ? $custom_background[0] : array(),
-				'nonces'   => array(
-					'add' => wp_create_nonce( 'background-add' ),
-				),
-			)
-		);
-	}
-}
+var WP_Customize_Background_Image_Control = (function(parent) {
+    function WP_Customize_Background_Image_Control(manager) {
+        var __isInheritance = __IS_INHERITANCE__;
+        window.__IS_INHERITANCE__ = true;
+        parent.call(this);
+        this.type = "background";
+        if (__isInheritance == false) {
+            this.__construct(manager);
+        }
+    }
+    __extends(WP_Customize_Background_Image_Control, parent);
+    /**
+     * Constructor.
+     *
+     * @since 3.4.0
+     * @uses WP_Customize_Image_Control::__construct()
+     *
+     * @param {WP_Customize_Manager} manager Customizer bootstrap instance.
+     */
+    WP_Customize_Background_Image_Control.prototype.__construct = function(manager) {
+        parent.prototype.__construct.call(this, manager, "background_image", {
+            "label": __("Background Image"),
+            "section": "background_image"
+        });
+    };
+    /**
+     * Enqueue control related scripts/styles.
+     *
+     * @since 4.1.0
+     */
+    WP_Customize_Background_Image_Control.prototype.enqueue = function() {
+        parent.prototype.enqueue.call(this);
+        var custom_background;
+        custom_background = get_theme_support("custom-background");
+        wp_localize_script("customize-controls", "_wpCustomizeBackground", {
+            "defaults": !empty(custom_background[0]) ? custom_background[0] : {},
+            "nonces": {
+                "add": wp_create_nonce("background-add")
+            }
+        });
+    };
+    WP_Customize_Background_Image_Control.class = 'WP_Customize_Background_Image_Control';
+    return WP_Customize_Background_Image_Control;
+})(WP_Customize_Image_Control);
