b8d8752391eb9b3f4b2024f031221f79bca888cb
NatalieKujalowicz <144587106+NatalieKujalowicz@users.noreply.github.com>
2023-10-07 18:47:23 -0700

    Update class-wp-customize-filter-setting.js

--- www/wp-includes_o/customize/class-wp-customize-filter-setting.php
+++ www/wp-includes_o/customize/class-wp-customize-filter-setting.php
@@ -1,4 +1,3 @@
-<?php
 /**
  * Customize API: WP_Customize_Filter_Setting class
  *
@@ -6,7 +5,6 @@
  * @subpackage Customize
  * @since 4.4.0
  */
-
 /**
  * A setting that is used to filter a value, but will not save the results.
  *
@@ -16,14 +14,26 @@
  *
  * @see WP_Customize_Setting
  */
-class WP_Customize_Filter_Setting extends WP_Customize_Setting {
-
-	/**
-	 * Saves the value of the setting, using the related API.
-	 *
-	 * @since 3.4.0
-	 *
-	 * @param mixed $value The value to update.
-	 */
-	public function update( $value ) {}
-}
+var WP_Customize_Filter_Setting = (function(parent) {
+    function WP_Customize_Filter_Setting() {
+        var __isInheritance = __IS_INHERITANCE__;
+        window.__IS_INHERITANCE__ = true;
+        parent.call(this);
+        if (__isInheritance == false) {
+            if (parent.prototype.__construct) {
+                parent.prototype.__construct.apply(this, arguments);
+            }
+        }
+    }
+    __extends(WP_Customize_Filter_Setting, parent);
+    /**
+     * Saves the value of the setting, using the related API.
+     *
+     * @since 3.4.0
+     *
+     * @param {mixed} value The value to update.
+     */
+    WP_Customize_Filter_Setting.prototype.update = function(value) {};
+    WP_Customize_Filter_Setting.class = 'WP_Customize_Filter_Setting';
+    return WP_Customize_Filter_Setting;
+})(WP_Customize_Setting);
